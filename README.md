0203af6184a913a05d454b2a198804a181ff21ab
ajuner <106791576@qq.com>
2021-02-22 14:44:37 +0800

    feat: create

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # vite-plugin-import
+
 use babel-plugin-import in vite
+
+## use
+
+```js
+// vite.config.js
+import { defineConfig } from "vite";
+import usePluginImport from './src/index.js'
+export default defineConfig({
+  plugins: [
+    ... // other plugins
+    usePluginImport({
+      libraryName: "ant-design-vue",
+      libraryDirectory: "es",
+      style: "css",
+    }),
+  ];
+})
+
+```
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import babelImport from 'babel-plugin-import';
+import babel from '@babel/core';
+import importMeta from '@babel/plugin-syntax-import-meta';
+
+function usePluginImport(options){
+
+  return {
+    name: 'vite-plugin-import',
+
+    transform(code, id) {
+
+      if (/.[j|t][sx|s]|vue$/.test(id)) {
+        const plugins = [importMeta, [babelImport, options]]
+
+        const result = babel.transformSync(code, {
+          ast: true,
+          plugins,
+          sourceFileName: id
+        })
+
+        return {
+          code: result.code,
+          map: result.map
+        }
+      }
+    },
+  };
+};
+
+export default usePluginImport
